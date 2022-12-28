cd3d7a0a730c50af0f8c3924564995f49c541e9c
Ofek Abramovitch <ofek301156@gmail.com>
2022-12-28 15:46:46 +0200

    Added noteSrvice

--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,11 +1,12 @@
 import { NoteHeader } from "../cmps/note-header";
 import { NoteList } from "../cmps/note-list";
 
+import { noteService } from "../services/note.service";
+
 export function NoteIndex() {
 
     return <section>
         <NoteHeader />
-        note app
         <NoteList />
     </section>
 
