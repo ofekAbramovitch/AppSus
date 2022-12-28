351172cac448606c95b6e4c51a0bf024e8895670
Ofek Abramovitch <ofek301156@gmail.com>
2022-12-28 22:32:47 +0200

    edited notes

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -1,16 +1,33 @@
 import { NotePreview } from './note-preview.jsx'
 
-export function NoteList({ notes }) {
+import { noteService } from "../services/note.service.js"
+
+export function NoteList({ notes, setNotes }) {
     if (!notes || !notes.length) return <div>Loading...</div>
 
-    return (
-        <ul className="note-list">
-    {
-        notes.map(note => <li key={note.id}>dfgdfgdfg
-        
-            <MailPreview note={note} />
-        </li>)
+    function onRemove(noteId) {
+         noteService.remove(noteId)
+            .then(() => setNotes())
     }
-    </ul>
+
+    return (
+        <ul className="note-list clean-list">
+            {
+                notes.map(note => <li key={note.id}>
+                    <NotePreview note={note} />
+                    <div>
+                        <button className='btn btn-rnd-s'>
+                            <i className='fa-solid fa-pencil'></i>
+                        </button>
+                        <button className='btn btn-rnd-s'>
+                            <i className='fa-solid fa-palette'></i>
+                        </button>
+                        <button onClick={() => onRemove(note.id)}><i className="fa-solid fa-trash"></i></button>
+                        <button><i className="fa-solid fa-thumbtack"></i></button>
+                    </div>
+                </li >
+                )
+            }
+        </ul >
     )
 }
