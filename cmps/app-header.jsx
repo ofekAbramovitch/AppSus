934046c86420fddb10822df120842672d5674036
Idan David <idand2000@gmail.com>
2022-12-31 20:02:41 +0200

    change header

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -9,17 +9,15 @@ export function AppHeader({ screenWidth }) {
 
     return <header className="app-header">
         <div className="header-content layout">
-            <Link to="/">
-                <div className="logo">
+            <Link className="logo" to="/">
                     <img src="./assets/img/home-logo.png" alt="" />
                     <h3>Appsus</h3>
-                </div>
             </Link>
             {screenWidth <= 481 && <i onClick={toggleMenu} className="fa-solid fa-bars"></i>}
             {(screenWidth > 481 || isMenuOpen) && <nav style={{ transform: "translateX(0)" }}>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/about">About</NavLink>
-                <NavLink to="/mail">Mail</NavLink>
+                <NavLink to="/mail/inbox">Mail</NavLink>
                 <NavLink to="/note">Note</NavLink>
             </nav>}
         </div>
